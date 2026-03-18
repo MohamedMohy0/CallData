a470340326a16e128b4ed0b9b2f4363959dbdb20
MohamedMohy0 <mohmmadmohy52@gmail.com>
2026-03-18 22:47:04 +0200

    V1

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL
-const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY
+const supabaseUrl = "https://egvmqirnnvieythrvrim.supabase.co"
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVndm1xaXJubnZpZXl0aHJ2cmltIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM4NDQ5OTIsImV4cCI6MjA4OTQyMDk5Mn0.lJZbWSE8WLJaqQcs2mnDCG28Rpt9xFhpfxeymO2anmY"
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
